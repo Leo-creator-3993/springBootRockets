aaee9ae9d9c918690be9596c69e4abb330ca78cd
leoCreator3993 <jkl2324@protonmail.com>
2024-11-09 21:51:59 +0800

    first commit

---
+++ springBootRockets/climbx1/src/test/java/com/future/springboot/rocket/climbx1/Clambx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.springboot.rocket.climbx1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clambx1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
